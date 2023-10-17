3bd74875937e6a6a0efa9edd9f1ea4e1192fba75
Mati-Mar <matias.marchesi03@gmail.com>
2023-10-17 20:34:21 -0300

    Se agrega proyecto base de comunicacion UART hecho en clase

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,119 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QByteArray>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    Port = NULL;    //indica que el objeto puerto no esta creado;
+    Portname = "";
+    EnumerarPuertos();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete Port;
+}
+
+void MainWindow::EnumerarPuertos()
+{
+    ui->comboBoxPort->clear();
+
+    QList<QSerialPortInfo> ports = QSerialPortInfo::availablePorts();
+
+    for (int i = 0; i < ports.size(); i++)
+    {
+        ui->comboBoxPort->addItem(ports.at(i).portName(), ports.at(i).portName());
+    }
+}
+
+void MainWindow::on_pushButtonConectar_clicked()
+{
+    if (!Port)
+    {
+        Port = new QSerialPort(Portname);
+        Port->setBaudRate(QSerialPort::Baud9600);
+        Port->setFlowControl(QSerialPort::NoFlowControl);
+        Port->setParity(QSerialPort::NoParity);
+        Port->setDataBits(QSerialPort::Data8);
+        Port->setStopBits(QSerialPort::OneStop);
+        if(!Port->open(QIODevice::ReadWrite))
+        {
+            QMessageBox::critical(this,"Error","No se puede abrir el puerto "+Port->portName());
+            delete Port;
+            Port = NULL;
+        }
+        else
+        {
+            ui->pushButtonConectar->setText("Desconectar");
+            connect(Port,SIGNAL(readyRead()),this,SLOT(on_Port_rx()));
+        }
+    }
+    else
+    {
+        delete Port;
+        Port = NULL;
+        ui->pushButtonConectar->setText("Conectar");
+    }
+}
+
+void MainWindow::on_pushButtonEnviar_clicked()
+{
+    if(Port)
+    {
+        QByteArray aux;
+        aux.append(ui->plainTextEditTX->toPlainText());
+        Port->write(aux);
+
+        ui->plainTextEditTX->clear();
+    }
+    else
+    {
+        QMessageBox::critical(this,"Error",QString::fromLatin1("No hay ningún puerto abierto"));
+    }
+}
+
+void MainWindow::on_Port_rx()
+{
+    QByteArray aux;
+    aux.resize(Port->bytesAvailable());
+    Port->read(aux.data(),aux.size());
+    ui->plainTextEditRX->appendPlainText(aux);
+}
+
+void MainWindow::on_comboBoxPort_currentIndexChanged(int index)
+{
+    Portname = ui->comboBoxPort->itemData(index).toString();    //recupero la data del item de indice index
+}
+
+void MainWindow::on_LedRojoPushButton_clicked()
+{
+    if(Port)
+        Port->write("#R$" - '\0');
+    else
+        QMessageBox::critical(this,"Error",QString::fromLatin1("No hay ningún puerto abierto"));
+}
+
+
+void MainWindow::on_LedVerdePushButton_clicked()
+{
+    if(Port)
+        Port->write("#V$" - '\0');
+    else
+        QMessageBox::critical(this,"Error",QString::fromLatin1("No hay ningún puerto abierto"));
+}
+
+
+void MainWindow::on_LedAzulPushButton_clicked()
+{
+    if(Port)
+        Port->write("#A$" - '\0');
+    else
+        QMessageBox::critical(this,"Error",QString::fromLatin1("No hay ningún puerto abierto"));
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,41 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void on_comboBoxPort_currentIndexChanged(int index);
+    void on_pushButtonEnviar_clicked();
+    void on_pushButtonConectar_clicked();
+    void on_Port_rx();
+
+    void on_LedRojoPushButton_clicked();
+
+    void on_LedVerdePushButton_clicked();
+
+    void on_LedAzulPushButton_clicked();
+
+private:
+    QString Portname;
+    //QextSerialPort *Port;
+    QSerialPort *Port;
+    void EnumerarPuertos();
 };
+
 #endif // MAINWINDOW_H
